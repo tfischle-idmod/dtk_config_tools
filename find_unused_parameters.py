127409d0d12223e645ae2ac6ef5aa43e851e91e5
INTERNAL\tfischle <tfischle@idmod.org>
2019-07-17 11:00:30 -0700

    output missing params per file, filter out parameters that are knowingly not in schema

--- find_unused_parameters.py
+++ find_unused_parameters.py
@@ -3,6 +3,8 @@ import argparse
 import os
 import json
 
+known_params_not_in_schema = set(['Default_Config_Path', 'Config_Name', 'Num_Cores', 'parameters', 'Python_Script_Path'])
+
 
 def getCommandLineArgs():
     parser = argparse.ArgumentParser(description='compare config file (e.g. config.json) to a reference of all valid parameters (e.g. schema.json)')
@@ -17,13 +19,13 @@ if __name__ == '__main__':
     commandline_args = getCommandLineArgs()
     WorkingDir = os.path.normpath(commandline_args.directory)
     name_config_file = commandline_args.file_name
-    name_config_file = ["config.json", "param_overrides.json"]
+    name_config_file = ["config.json", "param_overrides.json","campaign.json"]
 
     if commandline_args.r:
         file_list = []
         dirs = utils.getFilesFromSubDir(WorkingDir, name_config_file, file_list)
     else:
-        dirs = [WorkingDir + "\\" + name_config_file]
+        dirs = [os.path.join(WorkingDir, name_config_file)]
 
     schema_file = os.path.normcase(r"C:\\Users\\tfischle\\Desktop\\schema.json")
     with open(schema_file, 'r+') as f:
@@ -33,24 +35,25 @@ if __name__ == '__main__':
             print("Error could not load :", schema_file, "\n", e)
             exit(-1)
 
-    all_parameters = set()
-    parameters_not_in_schema = set()
-
-    for dir in dirs:
-        config_file = dir
+    for config_file in dirs:
+        params_in_file = set()
+        parameters_not_in_schema = set()
+        # print("Opening: ", config_file)
         with open(config_file, 'r+') as f:
             try:
-                j = json.load(f)
+                config = json.load(f)
             except Exception as e:
                 print("Error could not load :", config_file, "\n", e)
                 continue
 
-            utils.get_all_keys(j, all_parameters)
-            for param in all_parameters:
+            utils.get_all_keys(config, params_in_file)
+
+            for param in params_in_file:
                 if not utils.find_in_dict(schema, param):
                     #print ("Could not find in schema: ", param)
                     if "." not in param:
                         parameters_not_in_schema.add(param)
-
-    print ("Parameters used in configuration files: ", all_parameters)
-    print ("Parameters not in schema: ", parameters_not_in_schema)
+            print()
+            print(config_file, ":")
+            #print("parameters in config file: ", params_in_file )
+            print("parameters not in schema: ", parameters_not_in_schema.difference(known_params_not_in_schema))
