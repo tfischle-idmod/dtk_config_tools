6356071eac4e6782999e6d0a83bbdf4fea550222
INTERNAL\tfischle <tfischle@idmod.org>
2019-10-24 23:30:26 -0700

    improve args

--- find_unused_parameters.py
+++ find_unused_parameters.py
@@ -2,6 +2,7 @@ import utils
 import argparse
 import os
 import json
+import itertools
 
 known_params_not_in_schema = set(['Default_Config_Path', 'Config_Name', 'Num_Cores', 'parameters', 'Python_Script_Path'])
 
@@ -9,26 +10,25 @@ known_params_not_in_schema = set(['Default_Config_Path', 'Config_Name', 'Num_Cor
 def getCommandLineArgs():
     parser = argparse.ArgumentParser(description='compare config file (e.g. config.json) to a reference of all valid parameters (e.g. schema.json)')
     parser.add_argument("-r", action='store_true', default=False, help="recursive search in subdirectories")
-    parser.add_argument("directory", nargs='?', default=".", help="directory where the configuration file(s) to be converted are located (default = . )")
-    parser.add_argument("file_name",nargs='?', default="config.json", help="filename for file(s) that should be converted. With -r all files that end with file_name (e.g. config.json converts test_config.json) are found as well. (default = config.json)")
+    parser.add_argument("schema_file", help="Path to schema.")
+    parser.add_argument("directory", help="Directory where the configuration file(s) to be converted are located.")
+    parser.add_argument("file_name", nargs='?', default="config.json, param_overrides.json,campaign.json", help="filename for file(s) that should be converted. With -r all files that end with file_name (e.g. config.json converts test_config.json) are found as well. (default = config.json)")
     args = parser.parse_args()
     return args
-
 
 if __name__ == '__main__':
     commandline_args = getCommandLineArgs()
     WorkingDir = os.path.normpath(commandline_args.directory)
-    name_config_file = commandline_args.file_name
-    name_config_file = ["config.json", "param_overrides.json","campaign.json"]
+    name_config_file = commandline_args.file_name.split(',')
+    #name_config_file = ["config.json", "param_overrides.json","campaign.json"]
 
     if commandline_args.r:
         file_list = []
         dirs = utils.getFilesFromSubDir(WorkingDir, name_config_file, file_list)
     else:
-        dirs = [os.path.join(WorkingDir, name_config_file)]
+        dirs = [os.path.join(WorkingDir, n) for n in name_config_file]
 
-    schema_file = os.path.normcase(r"C:\\Users\\tfischle\\Desktop\\schema.json")
-    with open(schema_file, 'r+') as f:
+    with open(commandline_args.schema_file, 'r+') as f:
         try:
             schema = json.load(f)
         except Exception as e:
